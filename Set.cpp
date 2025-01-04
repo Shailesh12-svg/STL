1dd1bd35253561c6181e2c96ed0bb4a051fd383e
Shailesh <mallicksailesh957@gmail.com>
2025-01-04 22:31:16 +0530

    Set

---
+++ Set.cpp
@@ -0,0 +1,24 @@
+// These module will be contating all the types of Sets
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+void showSet()
+{
+    // Remember set is sorted and unique
+}
+
+void showMultiSet()
+{
+    // It is sorted but not unique
+}
+
+int main()
+
+{
+    showSet();
+    showMultiSet();
+
+    return 0;
+}
